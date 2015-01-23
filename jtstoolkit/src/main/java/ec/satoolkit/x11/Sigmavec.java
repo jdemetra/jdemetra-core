68dd613b09245e8e3f8dce43bf440c17df24f111
Christiane Hofer <Christiane.Hofer@bundesbank.de>
2015-01-23 17:41:56 +0100

    enum for sigmavec settings

---
+++ jtstoolkit/src/main/java/ec/satoolkit/x11/Sigmavec.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ec.satoolkit.x11;
+
+import ec.tstoolkit.design.Development;
+
+/**
+ *
+ * @author s4504ch
+ */
+@Development(status = Development.Status.Exploratory)
+public enum  Sigmavec {
+    group1,  
+    
+    group2;
+    
+    
+}
