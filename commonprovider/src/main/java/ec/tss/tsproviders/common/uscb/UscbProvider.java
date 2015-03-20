d4f19f39db02c66f3d29cd6f48ed15baa40e331a
Thomas Witthohn <thomas.witthohn@bundesbank.de>
2015-03-20 14:29:41 +0100

    Added reload method to IDataSourceProvider and implemented it in AbstractDataSourceProvider

--- commonprovider/src/main/java/ec/tss/tsproviders/common/uscb/UscbProvider.java
+++ commonprovider/src/main/java/ec/tss/tsproviders/common/uscb/UscbProvider.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2013 National Bank of Belgium
  *
- * Licensed under the EUPL, Version 1.1 or – as soon they will be approved 
+ * Licensed under the EUPL, Version 1.1 or – as soon they will be approved
  * by the European Commission - subsequent versions of the EUPL (the "Licence");
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
  *
  * http://ec.europa.eu/idabc/eupl
  *
- * Unless required by applicable law or agreed to in writing, software 
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Licence for the specific language governing permissions and 
+ * See the Licence for the specific language governing permissions and
  * limitations under the Licence.
  */
 package ec.tss.tsproviders.common.uscb;
@@ -251,7 +251,7 @@ public class UscbProvider implements ITsProvider, IDataSourceProvider {
             return false;
         }
     }
-    // --> ITSProvider    
+    // --> ITSProvider
 
     @Override
     public List<DataSet> children(DataSet parent) throws IllegalArgumentException, IOException {
@@ -261,5 +261,9 @@ public class UscbProvider implements ITsProvider, IDataSourceProvider {
     @Override
     public String getDisplayNodeName(DataSet dataSet) throws IllegalArgumentException {
         return getDisplayName(dataSet);
+    }
+
+    @Override
+    public void reload(DataSource dataSource) {
     }
 }
