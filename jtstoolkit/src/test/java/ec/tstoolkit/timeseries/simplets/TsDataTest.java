b1933f76291a7da5d3205e454b3ebe073fc5959e
Jean Palate <jean.palate@nbb.be>
2015-10-07 09:41:18 +0200

    Code cleanup

--- jtstoolkit/src/test/java/ec/tstoolkit/timeseries/simplets/TsDataTest.java
+++ jtstoolkit/src/test/java/ec/tstoolkit/timeseries/simplets/TsDataTest.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2013 National Bank of Belgium
  *
- * Licensed under the EUPL, Version 1.1 or – as soon they will be approved 
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved 
  * by the European Commission - subsequent versions of the EUPL (the "Licence");
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
