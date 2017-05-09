984a9fce080de027ea83f5b92d87ede6fcf65308
Jean Palate <jean.palate@nbb.be>
2017-05-09 10:48:12 +0200

    Handling of missing values in the likelihood statistics

--- jtss/src/main/java/ec/tss/html/implementation/HtmlLikelihood.java
+++ jtss/src/main/java/ec/tss/html/implementation/HtmlLikelihood.java
@@ -13,8 +13,7 @@
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the Licence for the specific language governing permissions and 
 * limitations under the Licence.
-*/
-
+ */
 package ec.tss.html.implementation;
 
 import ec.tss.html.AbstractHtmlElement;
@@ -24,7 +23,7 @@ import ec.tss.html.IHtmlElement;
 import java.io.IOException;
 
 /**
- * 
+ *
  * @author Jean Palate
  */
 public class HtmlLikelihood extends AbstractHtmlElement implements IHtmlElement {
@@ -32,42 +31,44 @@ public class HtmlLikelihood extends AbstractHtmlElement implements IHtmlElement 
     private final LikelihoodStatistics stats;
 
     /**
-     * 
+     *
      * @param stats
      */
-    public HtmlLikelihood(LikelihoodStatistics stats)
-    {
-	this.stats = stats;
+    public HtmlLikelihood(LikelihoodStatistics stats) {
+        this.stats = stats;
     }
 
     /**
-     * 
+     *
      * @param stream
      * @throws IOException
      */
     @Override
-    public void write(HtmlStream stream) throws IOException
-    {
-	stream.write("Number of effective observations = ").write(
-		stats.effectiveObservationsCount).newLine();
+    public void write(HtmlStream stream) throws IOException {
+        if (stats.missingCount > 0) {
+            stream.write("Number of missing observations = ").write(
+                    stats.missingCount).newLine();
+        }
+        stream.write("Number of effective observations = ").write(
+                stats.effectiveObservationsCount).newLine();
 
-	stream.write("Number of estimated parameters = ").write(
-		stats.estimatedParametersCount).newLines(2);
-	stream.write("Loglikelihood = ").write(stats.logLikelihood).newLine();
-	if (stats.transformationAdjustment != 0
-		&& !Double.isNaN(stats.transformationAdjustment)) {
-	    stream.write("Transformation adjustment = ").write(
-		    stats.transformationAdjustment).newLine();
-	    stream.write("Adjusted loglikelihood = ").write(
-		    stats.adjustedLogLikelihood).newLines(2);
-	}
-	double stde = Math.sqrt(stats.SsqErr / stats.effectiveObservationsCount);
-	stream.write("Standard error of the regression (ML estimate) = ")
-		.write(stde).newLine();
-	stream.write("AIC = ").write(stats.AIC).newLine();
-	stream.write("AICC = ").write(stats.AICC).newLine();
-	//stream.write("BIC = ").write(stats.BIC).newLine();
-	stream.write("BIC (corrected for length) = ").write(stats.BICC).newLine();
-	//stream.write("Hannan-Quinn = ").write(stats.HannanQuinn).newLines(2);
+        stream.write("Number of estimated parameters = ").write(
+                stats.estimatedParametersCount).newLines(2);
+        stream.write("Loglikelihood = ").write(stats.logLikelihood).newLine();
+        if (stats.transformationAdjustment != 0
+                && !Double.isNaN(stats.transformationAdjustment)) {
+            stream.write("Transformation adjustment = ").write(
+                    stats.transformationAdjustment).newLine();
+            stream.write("Adjusted loglikelihood = ").write(
+                    stats.adjustedLogLikelihood).newLines(2);
+        }
+        double stde = Math.sqrt(stats.SsqErr / stats.effectiveObservationsCount);
+        stream.write("Standard error of the regression (ML estimate) = ")
+                .write(stde).newLine();
+        stream.write("AIC = ").write(stats.AIC).newLine();
+        stream.write("AICC = ").write(stats.AICC).newLine();
+        //stream.write("BIC = ").write(stats.BIC).newLine();
+        stream.write("BIC (corrected for length) = ").write(stats.BICC).newLine();
+        //stream.write("Hannan-Quinn = ").write(stats.HannanQuinn).newLines(2);
     }
 }
