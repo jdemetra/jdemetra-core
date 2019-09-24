1213f01151880f37f83d85b02199bdd475f89229
Jean Palate <jean.palate@nbb.be>
2019-09-24 10:33:49 +0200

    SA-Experimental for R

--- demetra-sa-experimental/demetra-sa-experimental-r/src/main/java/demetra/saexperimental/r/FiltersToolkit.java
+++ demetra-sa-experimental/demetra-sa-experimental-r/src/main/java/demetra/saexperimental/r/FiltersToolkit.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package demetra.r;
+package demetra.saexperimental.r;
 
 import demetra.information.InformationMapping;
 import jdplus.maths.linearfilters.FiniteFilter;
--- demetra-sa-experimental/demetra-sa-experimental-r/src/main/java/demetra/saexperimental/r/LocalPolynomialFilters.java
+++ demetra-sa-experimental/demetra-sa-experimental-r/src/main/java/demetra/saexperimental/r/LocalPolynomialFilters.java
@@ -14,7 +14,7 @@
  * See the Licence for the specific language governing permissions and 
  * limitations under the Licence.
  */
-package demetra.r;
+package demetra.saexperimental.r;
 
 import jdplus.maths.linearfilters.FiniteFilter;
 import jdplus.maths.linearfilters.SymmetricFilter;
