8149786bc38d5b50d19eb2e0e28d84e8156b69c2
Jean Palate <jean.palate@nbb.be>
2019-09-24 10:33:49 +0200

    SA-Experimental for R

--- demetra-sa-experimental/demetra-sa-experimental-r/src/test/java/demetra/saexperimental/r/LocalPolynomialFiltersTest.java
+++ demetra-sa-experimental/demetra-sa-experimental-r/src/test/java/demetra/saexperimental/r/LocalPolynomialFiltersTest.java
@@ -14,7 +14,7 @@
  * See the Licence for the specific language governing permissions and 
  * limitations under the Licence.
  */
-package demetra.r;
+package demetra.saexperimental.r;
 
 import demetra.data.Data;
 import org.junit.Test;
