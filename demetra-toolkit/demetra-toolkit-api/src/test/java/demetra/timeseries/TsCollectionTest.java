06235c8818429ba746db6bb68562dadae6b055e7
Philippe Charles <philippe.charles@nbb.be>
2021-05-11 17:24:35 +0200

    Add TsCollection collector

---
+++ demetra-toolkit/demetra-toolkit-api/src/test/java/demetra/timeseries/TsCollectionTest.java
@@ -0,0 +1,27 @@
+package demetra.timeseries;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TsCollectionTest {
+
+    @Test
+    public void testToTsCollection() {
+        assertThat(Stream.<Ts>empty().collect(TsCollection.toTsCollection()))
+                .isEqualTo(TsCollection.EMPTY);
+
+        List<Ts> list = IntStream.range(0, 100).mapToObj(i -> Ts.builder().name("ts" + i).build()).collect(Collectors.toList());
+
+        assertThat(list.stream().collect(TsCollection.toTsCollection()))
+                .isEqualTo(TsCollection.builder().data(list).build());
+
+        assertThat(list.parallelStream().collect(TsCollection.toTsCollection()))
+                .isEqualTo(TsCollection.builder().data(list).build());
+    }
+}
