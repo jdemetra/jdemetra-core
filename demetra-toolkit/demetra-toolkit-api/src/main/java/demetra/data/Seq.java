112b339db919d50f5ca76552b2e87186d0124b15
Philippe Charles <philippe.charles@nbb.be>
2021-05-28 12:21:34 +0200

    Code cleanup

--- demetra-toolkit/demetra-toolkit-api/src/main/java/demetra/data/Seq.java
+++ demetra-toolkit/demetra-toolkit-api/src/main/java/demetra/data/Seq.java
@@ -1,39 +1,42 @@
 /*
  * Copyright 2021 National Bank of Belgium
- * 
- * Licensed under the EUPL, Version 1.2 or – as soon they will be approved 
+ *
+ * Licensed under the EUPL, Version 1.2 or – as soon they will be approved
  * by the European Commission - subsequent versions of the EUPL (the "Licence");
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * https://joinup.ec.europa.eu/software/page/eupl
- * 
- * Unless required by applicable law or agreed to in writing, software 
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Licence for the specific language governing permissions and 
+ * See the Licence for the specific language governing permissions and
  * limitations under the Licence.
  */
 package demetra.data;
 
-import nbbrd.design.Development;
 import internal.data.InternalSeq;
 import internal.data.InternalSeqCursor;
+import nbbrd.design.Development;
+import org.checkerframework.checker.index.qual.NonNegative;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.function.Consumer;
 import java.util.function.IntFunction;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
-import org.checkerframework.checker.index.qual.NonNegative;
-import org.checkerframework.checker.nullness.qual.NonNull;
 
 /**
  * Describes a generic sequence of elements.
  *
- * @author Philippe Charles
  * @param <E>
+ * @author Philippe Charles
  */
 @Development(status = Development.Status.Release)
 public interface Seq<E> extends BaseSeq, Iterable<E> {
@@ -46,7 +49,7 @@ public interface Seq<E> extends BaseSeq, Iterable<E> {
      * @param index the index of the value to be returned
      * @return the specified value
      * @throws IndexOutOfBoundsException if the <tt>index</tt> argument is
-     * negative or not less than <tt>length()</tt>
+     *                                   negative or not less than <tt>length()</tt>
      */
     E get(@NonNegative int index) throws IndexOutOfBoundsException;
 
@@ -70,13 +73,29 @@ public interface Seq<E> extends BaseSeq, Iterable<E> {
         return Spliterators.spliterator(iterator(), length(), 0);
     }
 
-    @NonNull
-    default Stream<E> stream() {
+    /**
+     * Returns a sequential Stream with this sequence as its source.
+     *
+     * @return a sequential Stream over the elements in this sequence
+     * @see Collection#stream()
+     */
+    default @NonNull Stream<E> stream() {
         return StreamSupport.stream(spliterator(), false);
     }
 
-    @NonNull
-    default E[] toArray(@NonNull IntFunction<E[]> generator) {
+    /**
+     * Returns an array containing all of the elements in this sequence,
+     * using the provided generator function to allocate the returned array.
+     *
+     * @param generator a function which produces a new array of the desired type and the provided length
+     * @return an array containing all of the elements in this sequence
+     * @see Collection#toArray(IntFunction)
+     */
+    default @NonNull E[] toArray(@NonNull IntFunction<E[]> generator) {
         return InternalSeq.toArray(this, generator);
+    }
+
+    default int indexOf(@NonNull Predicate<? super E> predicate) {
+        return InternalSeq.firstIndexOf(this, predicate);
     }
 }
--- demetra-toolkit/demetra-toolkit-api/src/main/java/internal/data/InternalSeq.java
+++ demetra-toolkit/demetra-toolkit-api/src/main/java/internal/data/InternalSeq.java
@@ -1,26 +1,28 @@
 /*
  * Copyright 2017 National Bank of Belgium
- * 
- * Licensed under the EUPL, Version 1.1 or - as soon they will be approved 
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
  * by the European Commission - subsequent versions of the EUPL (the "Licence");
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * http://ec.europa.eu/idabc/eupl
- * 
- * Unless required by applicable law or agreed to in writing, software 
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Licence for the specific language governing permissions and 
+ * See the Licence for the specific language governing permissions and
  * limitations under the Licence.
  */
 package internal.data;
+
+import demetra.data.Seq;
 
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.function.Consumer;
 import java.util.function.IntFunction;
-import demetra.data.Seq;
+import java.util.function.Predicate;
 
 /**
  * Support class of Sequence.
@@ -63,5 +65,14 @@ public class InternalSeq {
             result[i] = seq.get(i);
         }
         return result;
+    }
+
+    public <E> int firstIndexOf(Seq<E> seq, Predicate<? super E> predicate) {
+        for (int i = 0; i < seq.length(); i++) {
+            if (predicate.test(seq.get(i))) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
