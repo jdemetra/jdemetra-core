63ee5af540babedf78d6d6c57cd5da71f48f4099
palatej <jean.palate@nbb.be>
2021-10-22 15:56:19 +0200

    Ucarima in R

--- demetra-toolkit/demetra-toolkit-r/src/main/java/demetra/arima/r/UcarimaModels.java
+++ demetra-toolkit/demetra-toolkit-r/src/main/java/demetra/arima/r/UcarimaModels.java
@@ -8,11 +8,18 @@ package demetra.arima.r;
 import demetra.arima.ArimaModel;
 import demetra.arima.UcarimaModel;
 import demetra.data.DoubleSeq;
+import demetra.math.matrices.Matrix;
 import java.util.function.DoubleUnaryOperator;
+import jdplus.math.matrices.FastMatrix;
 import jdplus.modelling.ApiUtility;
+import jdplus.ssf.dk.DkToolkit;
+import jdplus.ssf.implementations.CompositeSsf;
+import jdplus.ssf.univariate.DefaultSmoothingResults;
+import jdplus.ssf.univariate.SsfData;
 import jdplus.ucarima.WienerKolmogorovEstimator;
 import jdplus.ucarima.WienerKolmogorovEstimators;
 import jdplus.ucarima.WienerKolmogorovPreliminaryEstimatorProperties;
+import jdplus.ucarima.ssf.SsfUcarima;
 
 /**
  *
@@ -20,11 +27,11 @@ import jdplus.ucarima.WienerKolmogorovPreliminaryEstimatorProperties;
  */
 @lombok.experimental.UtilityClass
 public class UcarimaModels {
-
+    
     public UcarimaModel of(ArimaModel model, ArimaModel[] components) {
         return new UcarimaModel(model, components);
     }
-
+    
     public UcarimaModel doCanonical(UcarimaModel ucm, int cmp, boolean adjust) {
         jdplus.ucarima.UcarimaModel m = ApiUtility.fromApi(ucm);
         jdplus.ucarima.UcarimaModel nm = m.setVarianceMax(cmp, adjust);
@@ -33,19 +40,19 @@ public class UcarimaModels {
         for (int i = 0; i < ncmps; ++i) {
             names[i] = ucm.getComponent(i).getName();
         }
-        if (ncmps<names.length){
-            names[ncmps]="noise";
+        if (ncmps < names.length) {
+            names[ncmps] = "noise";
         }
         return ApiUtility.toApi(nm, names);
     }
-
+    
     public double[] wienerKolmogorovFilter(UcarimaModel ucarima, int cmp, boolean signal, int nweights) {
         jdplus.ucarima.UcarimaModel ucm = ApiUtility.fromApi(ucarima);
         WienerKolmogorovEstimators wks = new WienerKolmogorovEstimators(ucm);
         WienerKolmogorovEstimator wk = wks.finalEstimator(cmp, signal);
         return DoubleSeq.onMapping(nweights, wk.getWienerKolmogorovFilter().weights()).toArray();
     }
-
+    
     public double[] wienerKolmogorovFilterGain(UcarimaModel ucarima, int cmp, boolean signal, int n) {
         jdplus.ucarima.UcarimaModel ucm = ApiUtility.fromApi(ucarima);
         WienerKolmogorovEstimators wks = new WienerKolmogorovEstimators(ucm);
@@ -59,16 +66,16 @@ public class UcarimaModels {
         }
         return g;
     }
-
+    
     public WienerKolmogorovEstimators wienerKolmogorovEstimators(UcarimaModel ucarima) {
         jdplus.ucarima.UcarimaModel ucm = ApiUtility.fromApi(ucarima);
         return new WienerKolmogorovEstimators(ucm);
     }
-
+    
     public WienerKolmogorovEstimator finalEstimator(WienerKolmogorovEstimators wk, int cmp, boolean signal) {
         return wk.finalEstimator(cmp, signal);
     }
-
+    
     public double[] gain(WienerKolmogorovEstimator wk, int n) {
         DoubleUnaryOperator gain = wk.getWienerKolmogorovFilter().gainFunction();
         double[] g = new double[n];
@@ -79,11 +86,11 @@ public class UcarimaModels {
         }
         return g;
     }
-
+    
     public double[] filter(WienerKolmogorovEstimator wk, int n) {
         return DoubleSeq.onMapping(n + 1, wk.getWienerKolmogorovFilter().weights()).toArray();
     }
-
+    
     public double[] spectrum(WienerKolmogorovEstimator wk, int n) {
         DoubleUnaryOperator s = wk.getEstimatorModel().getSpectrum().asFunction();
         double[] g = new double[n];
@@ -94,11 +101,29 @@ public class UcarimaModels {
         }
         return g;
     }
-
+    
     public WienerKolmogorovPreliminaryEstimatorProperties preliminaryEstimators(WienerKolmogorovEstimators wk, int cmp, boolean signal) {
         WienerKolmogorovPreliminaryEstimatorProperties wkp = new WienerKolmogorovPreliminaryEstimatorProperties(wk);
         wkp.select(cmp, signal);
         return wkp;
     }
-
+    
+    public Matrix estimate(double[] data, UcarimaModel ucarima, boolean stdev) {
+        jdplus.ucarima.UcarimaModel ucm = ApiUtility.fromApi(ucarima);
+        ucm = ucm.simplify();
+        CompositeSsf ssf = SsfUcarima.of(ucm);
+        DefaultSmoothingResults rslt = DkToolkit.sqrtSmooth(ssf, new SsfData(data), stdev, true);
+        int n = ucm.getComponentsCount();
+        FastMatrix M = FastMatrix.make(data.length, stdev ? 2 * n : n);
+        int[] pos = ssf.componentsPosition();
+        for (int i = 0; i < n; ++i) {
+            M.column(i).copy(rslt.getComponent(pos[i]));
+            if (stdev) {
+                M.column(n + i).copy(rslt.getComponentVariance(pos[i]).fastOp(w -> w <= 0 ? 0 : Math.sqrt(w)));
+            }
+        }
+        
+        return M;
+    }
+    
 }
---
+++ demetra-toolkit/demetra-toolkit-r/src/test/java/demetra/arima/r/UcarimaModelsTest.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package demetra.arima.r;
+
+import demetra.data.Data;
+import jdplus.sarima.SarimaModel;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author PALATEJ
+ */
+public class UcarimaModelsTest {
+    
+    public UcarimaModelsTest() {
+    }
+
+     
+}
