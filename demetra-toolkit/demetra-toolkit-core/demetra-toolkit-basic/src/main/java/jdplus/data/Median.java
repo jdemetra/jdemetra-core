993a091dde69f5bbb9e6165b6ed0e36b8c3bc538
palatej <jean.palate@nbb.be>
2022-04-22 10:33:58 +0200

    Median

--- demetra-toolkit/demetra-toolkit-core/demetra-toolkit-basic/src/main/java/jdplus/data/Median.java
+++ demetra-toolkit/demetra-toolkit-core/demetra-toolkit-basic/src/main/java/jdplus/data/Median.java
@@ -5,6 +5,7 @@
 package jdplus.data;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  *
@@ -13,140 +14,181 @@ import java.util.Arrays;
 @lombok.experimental.UtilityClass
 public class Median {
 
-//    int getPivot(int[] nums, int start, int end) {
-//        int[] medians = new int[(end - start) / 5];
-//        int i = start;
-//        int k = 0;
-//        for (int j = start + 5; j < end; i = j, j += 5) {
-//            Arrays.sort(nums, i, j);
-//            medians[k++] = nums[i + 2];
-//        }
-//
-//        // The magic!!! we are calling our own primary method recursively to find the median of medians.
-//        return kthSmallest(medians, 0, medians.length, medians.length / 2 + 1);
-//    }
-//
-//    int arrangeByPivot(int[] nums, int start, int end, int pivot) {
-//        int b = start;
-//        int e = end - 1;
-//        for (int i = start; i <= e;) {
-//            if (nums[i] < pivot) {
-//                int temp = nums[i];
-//                nums[i] = nums[b];
-//                nums[b] = temp;
-//                b++;
-//                i++;
-//            } else if (nums[i] == pivot) {
-//                i++;
-//            } else {
-//                int temp = nums[i];
-//                nums[i] = nums[e];
-//                nums[e] = temp;
-//                e--;
-//            }
-//        }
-//
-//        return b;
-//    }
-//
-//    int kthSmallest(int[] nums, int start, int end, int elm) {
-//        if (end - start <= 5) {
-//            Arrays.sort(nums, start, end);
-//            return nums[start + elm - 1];
-//        }
-//
-//        // find the pivot element that will split the 
-//        // set as evenly as possible - this is where all the magic happens
-//        int pivot = getPivot(nums, start, end);
-//
-//        // perform a partition based on the pivot found.
-//        int loc = arrangeByPivot(nums, start, end, pivot);
-//
-//        // check where is the element we are seeking compared to the partition index
-//        // call recursively depending on the partitioned location or return, if we have found the element
-//        if (loc - start == elm - 1) {
-//            return nums[loc];
-//        } else if (loc - start > elm - 1) {
-//            return kthSmallest(nums, start, loc, elm);
-//        }
-//
-//        return kthSmallest(nums, loc + 1, end, (elm - (loc + 1 - start)));
-//    }
-//
-//    public int findKthLargest(int[] nums, int k) {
-//        return kthSmallest(nums, 0, nums.length, nums.length - k + 1);
-//    }
-    int partition(int arr[], int low, int high) {
-        int pivot = arr[high];          //taken pivot element as last element
-        int z = (low - 1);
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {                                  //arranging all elements that are less than pivot
-                z++;
-                int temp = arr[z];
-                arr[z] = arr[j];
-                arr[j] = temp;
+    private int getPivot(int[] nums, int start, int end) {
+        int[] medians = new int[(end - start) / 5];
+        int i = start;
+        int k = 0;
+        for (int j = start + 5; j < end; i = j, j += 5) {
+            Arrays.sort(nums, i, j);
+            medians[k++] = nums[i + 2];
+        }
+        return kthSmallest(medians, 0, medians.length, medians.length / 2);
+    }
+
+    int arrangeByPivot(int[] nums, int start, int end, int pivot) {
+        int b = start;
+        int e = end - 1;
+        for (int i = start; i <= e;) {
+            if (nums[i] < pivot) {
+                int temp = nums[i];
+                nums[i] = nums[b];
+                nums[b] = temp;
+                b++;
+                i++;
+            } else if (nums[i] == pivot) {
+                i++;
+            } else {
+                int temp = nums[i];
+                nums[i] = nums[e];
+                nums[e] = temp;
+                e--;
             }
         }
-        int temp = arr[z + 1];
-        arr[z + 1] = arr[high];     //finalizing th position of pivot element in array which is sorted position
-        arr[high] = temp;
 
-        return z + 1;
+        return b;
+    }
+
+    private int kthSmallest(int[] nums, int start, int end, int elm) {
+        if (end - start <= 5) {
+            Arrays.sort(nums, start, end);
+            return nums[elm];
+        }
+
+        // find the pivot element that will split the 
+        // set as evenly as possible - this is where all the magic happens
+        int pivot = getPivot(nums, start, end);
+
+        // perform a partition based on the pivot found.
+        int loc = arrangeByPivot(nums, start, end, pivot);
+
+        // check where is the element we are seeking compared to the partition index
+        // call recursively depending on the partitioned location or return, if we have found the element
+        if (loc == elm) {
+            return nums[loc];
+        } else if (loc > elm) {
+            return kthSmallest(nums, start, loc, elm);
+        } else {
+            return kthSmallest(nums, loc + 1, end, elm);
+        }
+    }
+
+    public int findKthLargest(int[] nums, int k) {
+        return kthSmallest(nums, 0, nums.length, nums.length - k - 1);
+    }
+
+    public int findKthSmallest(int[] nums, int k) {
+        if (k < 0 || k >= nums.length) {
+            throw new IllegalArgumentException();
+        }
+        return kthSmallest(nums, 0, nums.length, k);
+    }
+
+    private double getPivot(double[] nums, int start, int end) {
+        double[] medians = new double[(end - start) / 5];
+        int i = start;
+        int k = 0;
+        for (int j = start + 5; j < end; i = j, j += 5) {
+            Arrays.sort(nums, i, j);
+            medians[k++] = nums[i + 2];
+        }
+        return kthSmallest(medians, 0, medians.length, medians.length / 2);
+    }
+
+    private int arrangeByPivot(double[] nums, int start, int end, double pivot) {
+        int b = start, i = start;
+        int e = end;
+        while (i < e) {
+            if (nums[i] < pivot) {
+                double temp = nums[i];
+                nums[i] = nums[b];
+                nums[b] = temp;
+                b++;
+                i++;
+            } else if (nums[i] == pivot) {
+                i++;
+            } else {
+                e--;
+                double temp = nums[i];
+                nums[i] = nums[e];
+                nums[e] = temp;
+            }
+        }
+
+        return b;
+    }
+
+    private double kthSmallest(double[] nums, int start, int end, int elm) {
+        if (end - start <= 5) {
+            Arrays.sort(nums, start, end);
+            return nums[elm];
+        }
+
+        // find the pivot element that will split the 
+        // set as evenly as possible - this is where all the magic happens
+        double pivot = getPivot(nums, start, end);
+
+        // perform a partition based on the pivot found.
+        int loc = arrangeByPivot(nums, start, end, pivot);
+
+        // check where is the element we are seeking compared to the partition index
+        // call recursively depending on the partitioned location or return, if we have found the element
+        if (loc == elm) {
+            return nums[loc];
+        } else if (loc > elm) {
+            return kthSmallest(nums, start, loc, elm);
+        } else {
+            return kthSmallest(nums, loc + 1, end, elm);
+        }
+    }
+
+    public double findKthLargest(double[] nums, int k) {
+        return kthSmallest(nums, 0, nums.length, nums.length - k - 1);
+    }
+
+    public double findKthSmallest(double[] nums, int k) {
+        if (k < 0 || k >= nums.length) {
+            throw new IllegalArgumentException();
+        }
+        return kthSmallest(nums, 0, nums.length, k);
     }
 
     public int kselection(int[] arr, int low,
             int high, int k) {
-        int partition_sorting_value = partition(arr, low, high);
-        if (partition_sorting_value == k) //comparing the position returned with the desired position k
-        {
+        int pivot = arr[RND.nextInt(low, high)];
+        int partition_sorting_value = arrangeByPivot(arr, low, high, pivot);
+        if (partition_sorting_value == k) {
             return arr[partition_sorting_value];
-        } else if (partition_sorting_value < k) //partition value is less than k search left half array
-        {
+        } else if (partition_sorting_value < k) {
             return kselection(arr, partition_sorting_value + 1, high, k);
-        } else //partition value is greater than k search right half array         
-        {
-            return kselection(arr, low, partition_sorting_value - 1, k);
+        } else {
+            return kselection(arr, low, partition_sorting_value, k);
         }
     }
 
     public int kselection(int[] arr, int k) {
-        return kselection(arr, 0, arr.length - 1, k);
-    }
-
-    int partition(double arr[], int low, int high) {
-        double pivot = arr[high];          //taken pivot element as last element
-        int z = (low - 1);
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {                                  //arranging all elements that are less than pivot
-                z++;
-                double temp = arr[z];
-                arr[z] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        double temp = arr[z + 1];
-        arr[z + 1] = arr[high];     //finalizing th position of piviot element in array which is sorted position
-        arr[high] = temp;
-
-        return z + 1;
+        return kselection(arr, 0, arr.length, k);
     }
 
     public double kselection(double[] arr, int low,
             int high, int k) {
-        int partition_sorting_value = partition(arr, low, high);
-        if (partition_sorting_value == k) //comparing the position returned with the desired position k
-        {
+
+        double pivot = arr[RND.nextInt(low, high)];
+
+//        int partition_sorting_value = partition(arr, low, high);
+        int partition_sorting_value = arrangeByPivot(arr, low, high, pivot);
+
+        if (partition_sorting_value == k) {
             return arr[partition_sorting_value];
-        } else if (partition_sorting_value < k) //partition value is less than k search left half array
-        {
+        } else if (partition_sorting_value < k) {
             return kselection(arr, partition_sorting_value + 1, high, k);
-        } else //partition value is greater than k search right half array         
-        {
-            return kselection(arr, low, partition_sorting_value - 1, k);
+        } else {
+            return kselection(arr, low, partition_sorting_value, k);
         }
     }
 
     public double kselection(double[] arr, int k) {
-        return kselection(arr, 0, arr.length - 1, k);
+        return kselection(arr, 0, arr.length, k);
     }
+
+    private static final Random RND = new Random(0);
 }
--- demetra-toolkit/demetra-toolkit-core/demetra-toolkit-basic/src/test/java/jdplus/data/MedianTest.java
+++ demetra-toolkit/demetra-toolkit-core/demetra-toolkit-basic/src/test/java/jdplus/data/MedianTest.java
@@ -14,19 +14,20 @@ import static org.junit.Assert.*;
  * @author PALATEJ
  */
 public class MedianTest {
-    
+
     public MedianTest() {
     }
 
     @Test
     public void testMedian() {
-        double[] nums=new double[1000];
-        Random rnd=new Random(0);
-        for (int i=0; i<nums.length; ++i)
-            nums[i]=rnd.nextDouble();
-        double[] nums2=nums.clone();
+        double[] nums = new double[1000];
+        Random rnd = new Random(0);
+        for (int i = 0; i < nums.length; ++i) {
+            nums[i] = rnd.nextDouble();
+        }
+        double[] nums2 = nums.clone();
 //        long t0=System.currentTimeMillis();
-        double k1 = Median.kselection(nums, nums.length/2);
+        double k1 = Median.kselection(nums, nums.length / 2);
 //        long t1=System.currentTimeMillis();
 //        System.out.println(t1-t0);
 //        System.out.println(k);
@@ -34,9 +35,67 @@ public class MedianTest {
         Arrays.sort(nums2);
 //        t1=System.currentTimeMillis();
 //        System.out.println(t1-t0);
-        double k2=nums2[nums.length/2];
+        double k2 = nums2[nums.length / 2];
 //        System.out.println();
-        assertTrue(k1==k2);
+        assertTrue(k1 == k2);
     }
-    
+
+    @Test
+    public void testLargest() {
+        int[] nums = new int[20];
+        Random rnd = new Random(0);
+        for (int i = 0; i < nums.length; ++i) {
+            nums[i] = rnd.nextInt(100);
+            //           System.out.println(nums[i]);
+        }
+//        System.out.println();
+
+        for (int i = 0; i < 20; ++i) {
+            int k = Median.findKthLargest(nums, i);
+            //           System.out.println(k);
+        }
+
+        for (int i = 0; i < 20; ++i) {
+            int k = Median.findKthSmallest(nums, i);
+//            System.out.println(k);
+        }
+    }
+
+    public static void main(String[] arg) {
+        int N = 10000, K = 1000, Q = N / 2;
+        double[] nums = new double[N];
+        Random rnd = new Random();
+        for (int i = 0; i < nums.length; ++i) {
+            nums[i] = rnd.nextDouble();
+        }
+        double k1 = 0, k2 = 0, k3 = 0;
+        long t0 = System.currentTimeMillis();
+        for (int j = 0; j < K; ++j) {
+            double[] c = nums.clone();
+            k1 = Median.findKthSmallest(c, Q);
+        }
+        long t1 = System.currentTimeMillis();
+        System.out.println(t1 - t0);
+        System.out.println(k1);
+        t0 = System.currentTimeMillis();
+        for (int j = 0; j < K; ++j) {
+            double[] c = nums.clone();
+            Arrays.sort(c);
+            k2 = c[Q];
+        }
+        t1 = System.currentTimeMillis();
+        System.out.println(t1 - t0);
+        System.out.println(k2);
+
+        t0 = System.currentTimeMillis();
+        for (int j = 0; j < K; ++j) {
+            double[] c = nums.clone();
+            k3 = Median.kselection(c, Q);
+        }
+        t1 = System.currentTimeMillis();
+        System.out.println(t1 - t0);
+        System.out.println(k3);
+
+    }
+
 }
