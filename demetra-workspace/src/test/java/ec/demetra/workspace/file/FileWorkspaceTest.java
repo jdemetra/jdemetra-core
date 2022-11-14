9f41c8c9cabe588aced7765b8b25cc0d46364747
Philippe Charles <philippe.charles@nbb.be>
2022-11-14 10:44:36 +0100

    Remove jimfs dependency

--- demetra-workspace/src/test/java/ec/demetra/workspace/file/FileWorkspaceTest.java
+++ demetra-workspace/src/test/java/ec/demetra/workspace/file/FileWorkspaceTest.java
@@ -1,23 +1,21 @@
 /*
  * Copyright 2017 National Bank of Belgium
- * 
- * Licensed under the EUPL, Version 1.1 or - as soon they will be approved 
+ *
+ * Licensed under the EUPL, Version 1.1 or - as soon they will be approved
  * by the European Commission - subsequent versions of the EUPL (the "Licence");
  * You may not use this work except in compliance with the Licence.
  * You may obtain a copy of the Licence at:
- * 
+ *
  * http://ec.europa.eu/idabc/eupl
- * 
- * Unless required by applicable law or agreed to in writing, software 
+ *
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licence is distributed on an "AS IS" basis,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the Licence for the specific language governing permissions and 
+ * See the Licence for the specific language governing permissions and
  * limitations under the Licence.
  */
 package ec.demetra.workspace.file;
 
-import com.google.common.jimfs.Configuration;
-import com.google.common.jimfs.Jimfs;
 import ec.demetra.workspace.Workspace;
 import ec.demetra.workspace.WorkspaceItem;
 import ec.satoolkit.tramoseats.TramoSeatsSpecification;
@@ -31,34 +29,18 @@ import ec.tstoolkit.modelling.arima.tramo.TramoSpecification;
 import ec.tstoolkit.modelling.arima.x13.RegArimaSpecification;
 import ec.tstoolkit.timeseries.calendars.GregorianCalendarManager;
 import ec.tstoolkit.timeseries.regression.TsVariables;
-import static internal.test.TestResources.GENERIC_INDEX;
-import static internal.test.TestResources.GENERIC_ITEMS;
-import static internal.test.TestResources.GENERIC_MOD_DOC_REGARIMA;
-import static internal.test.TestResources.GENERIC_MOD_DOC_TRAMO;
-import static internal.test.TestResources.GENERIC_MOD_SPEC_REGARIMA;
-import static internal.test.TestResources.GENERIC_MOD_SPEC_TRAMO;
-import static internal.test.TestResources.GENERIC_ROOT;
-import static internal.test.TestResources.GENERIC_SA_DOC_TRAMOSEATS;
-import static internal.test.TestResources.GENERIC_SA_DOC_X13;
-import static internal.test.TestResources.GENERIC_SA_MULTI;
-import static internal.test.TestResources.GENERIC_SA_SPEC_TRAMOSEATS;
-import static internal.test.TestResources.GENERIC_SA_SPEC_X13;
-import static internal.test.TestResources.GENERIC_UTIL_CAL;
-import static internal.test.TestResources.GENERIC_UTIL_VAR;
-import static internal.test.TestResources.LEGACY_INDEX;
-import static internal.test.TestResources.LEGACY_ROOT;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
 import java.io.IOException;
-import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
+import static internal.test.TestResources.*;
 import static org.assertj.core.api.Assertions.assertThat;
-import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 
 /**
- *
  * @author Philippe Charles
  */
 public class FileWorkspaceTest {
@@ -109,11 +91,11 @@ public class FileWorkspaceTest {
 
     @Test
     @SuppressWarnings("null")
-    public void testCreate() throws IOException {
+    public void testCreate(@TempDir Path temp) throws IOException {
         assertThatThrownBy(() -> FileWorkspace.create(null, FileFormat.GENERIC)).isInstanceOf(NullPointerException.class);
-        assertThatThrownBy(() -> FileWorkspace.create(JIM_FS.getPath("/"), null)).isInstanceOf(NullPointerException.class);
+        assertThatThrownBy(() -> FileWorkspace.create(temp, null)).isInstanceOf(NullPointerException.class);
         {
-            Path generic = Files.createTempFile(JIM_FS.getPath("/"), "ws_", "xml");
+            Path generic = Files.createTempFile(temp, "ws_", "xml");
 
             assertThatThrownBy(() -> FileWorkspace.create(generic, FileFormat.GENERIC)).isInstanceOf(IOException.class);
 
@@ -126,7 +108,7 @@ public class FileWorkspaceTest {
             }
         }
         {
-            Path legacy = Files.createTempFile(JIM_FS.getPath("/"), "ws_", "xml");
+            Path legacy = Files.createTempFile(temp, "ws_", "xml");
 
             assertThatThrownBy(() -> FileWorkspace.create(legacy, FileFormat.LEGACY)).isInstanceOf(IOException.class);
 
@@ -141,8 +123,8 @@ public class FileWorkspaceTest {
     }
 
     @Test
-    public void testCopyTo() throws IOException {
-        Path newFile = JIM_FS.getPath("/copied.xml");
+    public void testCopyTo(@TempDir Path temp) throws IOException {
+        Path newFile = temp.resolve("copied.xml");
         try (Workspace source = FileWorkspace.open(GENERIC_INDEX); Workspace target = FileWorkspace.create(newFile, FileFormat.GENERIC)) {
             source.copyTo(target);
         }
@@ -172,17 +154,5 @@ public class FileWorkspaceTest {
             assertThat(ws.load(GENERIC_UTIL_CAL)).isInstanceOf(GregorianCalendarManager.class);
             assertThat(ws.load(GENERIC_UTIL_VAR)).isInstanceOf(TsVariables.class);
         }
-    }
-
-    private static FileSystem JIM_FS;
-
-    @BeforeClass
-    public static void beforeClass() {
-        JIM_FS = Jimfs.newFileSystem(Configuration.unix());
-    }
-
-    @AfterClass
-    public static void afterClass() throws IOException {
-        JIM_FS.close();
     }
 }
