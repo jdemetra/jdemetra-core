be7752e7236a50f85e6f634e9767f07851c5aabe
Palate Jean <jean.palate@nbb.be>
2023-02-13 18:03:13 +0100

    Ssf spline

---
+++ demetra-sts/demetra-sts-core/src/main/java/jdplus/msts/internal/SplineItem.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jdplus.msts.internal;
+
+import jdplus.msts.StateItem;
+import demetra.data.DoubleSeq;
+import jdplus.msts.MstsMapping;
+import jdplus.msts.VarianceInterpreter;
+import java.util.Collections;
+import java.util.List;
+import jdplus.msts.ParameterInterpreter;
+import jdplus.ssf.ISsfLoading;
+import jdplus.ssf.StateComponent;
+import jdplus.ssf.sts.PeriodicComponent;
+import jdplus.ssf.sts.SplineComponent;
+
+/**
+ *
+ * @author palatej
+ */
+public class SplineItem extends StateItem {
+
+    private final VarianceInterpreter v;
+    private final int startpos;
+    private final SplineComponent.Data data;
+
+    public SplineItem(String name, int[] pos, int startpos, double cvar, boolean fixedvar) {
+        super(name);
+        v = new VarianceInterpreter(name + ".var", cvar, fixedvar, true);
+        this.data = SplineComponent.Data.of(pos);
+        this.startpos = startpos;
+    }
+
+    private SplineItem(SplineItem item) {
+        super(item.name);
+        this.data = item.data;
+        this.startpos = item.startpos;
+        v = item.v.duplicate();
+    }
+
+    @Override
+    public SplineItem duplicate() {
+        return new SplineItem(this);
+    }
+
+    @Override
+    public void addTo(MstsMapping mapping) {
+        mapping.add(v);
+        mapping.add((p, builder) -> {
+            double var = p.get(0);
+            builder.add(name, SplineComponent.stateComponent(data, var), SplineComponent.loading(data, startpos));
+            return 1;
+        });
+    }
+
+    @Override
+    public List<ParameterInterpreter> parameters() {
+        return Collections.singletonList(v);
+    }
+
+    @Override
+    public StateComponent build(DoubleSeq p) {
+        double var = p.get(0);
+        return SplineComponent.stateComponent(data, var);
+    }
+
+    @Override
+    public int parametersCount() {
+        return 1;
+    }
+
+    @Override
+    public ISsfLoading defaultLoading(int m) {
+        if (m > 0) {
+            return null;
+        }
+        return SplineComponent.loading(data, startpos);
+    }
+
+    @Override
+    public int defaultLoadingCount() {
+        return 1;
+    }
+
+    @Override
+    public int stateDim() {
+        return data.getDim();
+    }
+
+    @Override
+    public boolean isScalable() {
+        return !v.isFixed();
+    }
+
+}
--- demetra-toolkit/demetra-toolkit-core/demetra-toolkit-algorithms/src/test/java/internal/jdplus/math/functions/gsl/interpolation/CubicSplinesTest.java
+++ demetra-toolkit/demetra-toolkit-core/demetra-toolkit-algorithms/src/test/java/internal/jdplus/math/functions/gsl/interpolation/CubicSplinesTest.java
@@ -16,23 +16,69 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author PALATEJ
  */
 public class CubicSplinesTest {
-    
+
     public CubicSplinesTest() {
     }
 
-   @Test
+    @Test
     public void testSimple() {
-        double[] xi = new double[]{5, 10, 15, 20, 30, 50, 60, 75, 90, 120,130};
+        double[] xi = new double[]{5, 10, 15, 20, 30, 50, 60, 75, 90, 120, 130};
         double[] fxi = new double[]{-3, 20, -10, 5, 11, -3, 20, -10, 5, 4, -3};
         CubicSplines.Spline natural = CubicSplines.natural(DoubleSeq.of(xi), DoubleSeq.of(fxi));
         CubicSplines.Spline periodic = CubicSplines.periodic(DoubleSeq.of(xi), DoubleSeq.of(fxi));
         DoubleUnaryOperator fn = CubicSpline.of(xi, fxi);
         for (int i = 0; i <= 135; ++i) {
-            double f0=fn.applyAsDouble(i);
+            double f0 = fn.applyAsDouble(i);
             double f1 = natural.applyAsDouble(i);
             double f2 = periodic.applyAsDouble(i);
             assertEquals(f0, f1, 1e-6);
         }
     }
-    
+
+    @Test
+    public void testAdditive() {
+        double[] xi = new double[]{0, 10, 15, 20, 30, 50, 60, 75, 90, 120, 130};
+        double[] fxi = new double[]{-3, 20, -10, 5, 11, -3, 20, -10, 5, 4, -3};
+        CubicSplines.Spline periodic = CubicSplines.periodic(DoubleSeq.of(xi), DoubleSeq.of(fxi));
+        CubicSplines.Spline[] nodes = new CubicSplines.Spline[xi.length - 1];
+        for (int i = 0; i < nodes.length; ++i) {
+            double[] f = new double[xi.length];
+            if (i == 0) {
+                f[0] = 1;
+                f[nodes.length] = 1;
+            } else {
+                f[i] = 1;
+            }
+            nodes[i] = CubicSplines.periodic(DoubleSeq.of(xi), DoubleSeq.of(f));
+        }
+        for (int i = 0; i <= 130; ++i) {
+            double f = periodic.applyAsDouble(i);
+            double s = 0;
+            for (int j = 0; j < nodes.length; ++j) {
+                double z = nodes[j].applyAsDouble(i);
+                s += z * fxi[j];
+                System.out.print(z);
+                System.out.print('\t');
+            }
+            System.out.print(s);
+            System.out.print('\t');
+            System.out.println(f);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        long t0 = System.currentTimeMillis();
+        for (int j = 0; j < 10000; ++j) {
+            double[] xi = new double[]{5, 10, 15, 20, 30, 50, 60, 75, 90, 120, 130};
+            double[] fxi = new double[]{-3, 20, -10, 5, 11, -3, 20, -10, 5, 4, -3};
+            CubicSplines.Spline periodic = CubicSplines.periodic(DoubleSeq.of(xi), DoubleSeq.of(fxi));
+            for (int i = 0; i <= 13500; ++i) {
+                double f = periodic.applyAsDouble(0.01 * i);
+            }
+        }
+        long t1 = System.currentTimeMillis();
+        System.out.println(t1 - t0);
+    }
+
 }
